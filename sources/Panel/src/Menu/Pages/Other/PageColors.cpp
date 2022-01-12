fd2c447bd609eaeb4a95125ae008895481126bc2
Sasha7b9 <Sasha7b9@tut.by>
2022-01-12 11:55:41 +0300

    *

---
+++ sources/Panel/src/Menu/Pages/Other/PageColors.cpp
@@ -0,0 +1,6 @@
+// 2022/01/12 11:54:35 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Menu/Pages/Other/PageColors.h"
+
+
+
---
+++ sources/Panel/src/Menu/Pages/Other/PageColors.h
@@ -0,0 +1,5 @@
+// 2022/01/12 11:54:28 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+
