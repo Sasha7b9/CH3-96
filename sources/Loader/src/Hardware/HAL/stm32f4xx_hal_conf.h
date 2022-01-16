105fae41b5b372f0e3844e6bff614d70ffbc612e
Sasha7b9 <Sasha7b9@tut.by>
2022-01-16 18:05:26 +0300

    *

--- sources/Loader/src/Hardware/HAL/stm32f4xx_hal_conf.h
+++ sources/Loader/src/Hardware/HAL/stm32f4xx_hal_conf.h
@@ -21,7 +21,7 @@
   * @brief This is the list of modules to be used in the HAL driver 
   */
 #define HAL_MODULE_ENABLED  
-//#define HAL_DMA_MODULE_ENABLED
+#define HAL_DMA_MODULE_ENABLED
 #define HAL_FLASH_MODULE_ENABLED
 //#define HAL_SRAM_MODULE_ENABLED
 #define HAL_GPIO_MODULE_ENABLED
