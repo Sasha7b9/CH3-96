9b99bd7c6457b182a3b461931b9981218d7bdba5
Sasha7b9 <Sasha7b9@tut.by>
2022-01-17 19:54:47 +0300

    *

--- sources/VS/checksum/main.cpp
+++ sources/VS/checksum/main.cpp
@@ -1,7 +1,21 @@
 // 2022/1/17 19:45:07 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma warning(push, 0)
+#pragma warning(disable : 4668)
+#include <iostream>
+#pragma warning(pop)
 
 
-int main()
+using namespace std;
+
+
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        cout << "Using :" << endl;
+        cout << "        checsum input_file.bin" << endl;
+        return -1;
+    }
+
     return 0;
 }
